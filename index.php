9772c5812a1192b87d31485c85a679ddc17d28cc
sidra-ch <ms.sidrachaudhary@gmail.com>
2025-01-27 20:51:24 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <?php include './boot_css.php'?>
+  
+  <title>Etsy Pakistan ..Shop for handmade products</title>
+  <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/174/174847.png" type="image/x-icon">
+
+</head>
+<body>
+
+  <?php include 'nav.php'?>
+
+ 
+      <div class="col-xl-9 col-lg-10 col-11 mx-auto">
+    <div class="row py-xl-3 py-2 my-3">
+      <div class="col-xl-8 col-12">
+
+        <div class="card rounded-2  overflow-hidden border-0" style="height:100%">
+          <div class="row p-0 position-relative" style="background-color: #FFFBD8; height:100%">
+            <div class="col-sm-6 text-center d-flex flex-column align-items-center justify-content-center">
+              <h2>Renew your rooms!</h2>
+              <h4>Shop original home pieces</h4>
+              <button class="btn btn-dark shop-btn rounded-pill">Shop Now </button>
+          </div>
+          <div class="col-sm-6 circle-image pe-0">
+          <img width="100%" height="100%" src="./images/image 1.avif" alt="image-1" >
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="col-xl-4 d-none d-xl-block">
+        <div class="card border-0 rounded-2 overflow-hidden change-image" style="height:100%">
+        <img src="./images/image10.avif" width="100%" height="100%" alt="second-image">
+      <div class="overlay-card position-absolute h-100 w-100" style="background:linear-gradient(rgba(255,255,255,0), rgba(255,255,255,0)rgba(0,0,0,0.8 ));">
+</div>
+<div class="text-overlay text-white position-absolute" style="bottom:10px;left:10px;">
+<h2 class="img-text"></h2>
+<h3 class="img-para"></h3>
+</div>
+      </div>
+    </div>
+  </div>
+  </div>
+ 
+   <?php include './categories.php'?>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ <script src="./app.js"></script>
+ 
+</body>
+</html>
+
